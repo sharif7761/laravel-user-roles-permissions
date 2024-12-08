9bbe71b6eef46e3c0f51e3fb7f2be71600c35bd9
ahmed7761 <sharif7761@gmail.com>
2024-12-08 15:54:48 +0600

    laravel passport auth setup

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+        ], 201);
+
+    }
+
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('myToken')->accessToken;
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token
+        ]);
+    }
+
+    public function logout(){
+        auth()->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
+
+    public function profile(){
+        $user = Auth::user();
+        return response()->json([
+            "message" => "Profile info",
+            "user" => $user
+        ]);
+    }
+}
